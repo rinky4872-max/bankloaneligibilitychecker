4c191e4c3f52ae79e62f899bb3fc0557318aed76
Rajukumar <rinky4872@gmail.com>
2026-08-23 21:55:06 +0530

    Person details

---
+++ displayname.py
@@ -0,0 +1,12 @@
+Name ="S Rajukumar"
+print("My name is ",Name)
+Gender ="Male"
+print("Iam",Gender)
+Age = 40
+print("I am",Age,"years old.")
+Address = "Ramnaar,Hyderabad"
+print("I live in",Address)
+Mobile = "8978201111"
+print("My mobile number is",Mobile)
+Nationality = "Indian"
+print("I am an",Nationality)
